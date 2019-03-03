e513b3ac5aa756ba570f3fc57d7688b4d8fcbd80
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2019-03-03 19:04:30 +0000

    updated to use base controller

--- src/controller/HomeController.ts
+++ src/controller/HomeController.ts
@@ -1,19 +1,19 @@
 import * as express from 'express';
 import {inject} from 'inversify';
-import {controller, httpGet, interfaces, request, response} from 'inversify-express-utils';
+import {BaseHttpController, controller, httpGet, request, response} from 'inversify-express-utils';
 import TYPE from '../constant/TYPE';
 import {HomeService} from '../service/HomeService';
 
 @controller('/')
-export class HomeController implements interfaces.Controller {
+export class HomeController extends BaseHttpController {
 
     public constructor(@inject(TYPE.HomeService) private readonly homeService: HomeService) {
+        super();
         this.homeService = homeService;
     }
 
     @httpGet('/', TYPE.ProtectMiddleware)
     public async get(@request() req: express.Request, @response() res: express.Response): Promise<object> {
-
         return this.homeService.sayHello();
     }
 }
