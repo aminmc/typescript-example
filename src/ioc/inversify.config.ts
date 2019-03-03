ed0bf9284dd6a4174f73d474f2ee480c7086c572
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2019-03-03 10:56:31 +0000

    updated

--- src/ioc/inversify.config.ts
+++ src/ioc/inversify.config.ts
@@ -1,13 +1,11 @@
 import {Container} from 'inversify';
-import {buildProviderModule} from 'inversify-binding-decorators';
-import TYPE from '../constant/TYPE';
 import '../controller/HomeController';
+import TYPE from '../constant/TYPE';
 import {HomeService} from '../service/HomeService';
 import {KeycloakService} from '../service/KeycloakService';
 import {ProtectMiddleware} from '../service/ProtectMiddleware';
 
 const container = new Container();
-// container.load(buildProviderModule());
 container.bind<KeycloakService>(TYPE.KeycloakService).to(KeycloakService).inSingletonScope();
 container.bind<HomeService>(TYPE.HomeService).to(HomeService).inSingletonScope();
 container.bind<ProtectMiddleware>(TYPE.ProtectMiddleware).to(ProtectMiddleware).inSingletonScope();
