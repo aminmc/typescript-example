308ab8bc4d4e9f3e3c72d393bf25383f83f3fc91
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2019-03-10 08:58:54 +0000

    validation middleware

--- src/ioc/ApplicationContext.ts
+++ src/ioc/ApplicationContext.ts
@@ -1,5 +1,4 @@
 import {Container} from 'inversify';
-import {buildProviderModule} from 'inversify-binding-decorators';
 import TYPE from '../constant/TYPE';
 import {ProtectMiddleware} from '../middleware/ProtectMiddleware';
 import {HomeService} from '../service/HomeService';
@@ -17,7 +16,6 @@ export class ApplicationContext {
         this.container.bind<HomeService>(TYPE.HomeService).to(HomeService);
         this.container.bind<ProtectMiddleware>(TYPE.ProtectMiddleware).to(ProtectMiddleware);
         this.container.bind<Config>(TYPE.Config).to(Config);
-         // this.container.load(buildProviderModule());
     }
 
     public iocContainer(): Container {
